f9b3b37cae3e81c87141d0a2d27cf7057377cb80
iuridev <iuribarreto.ib@gmail.com>
2026-01-31 20:08:32 -0300

    corrigir rota

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -44,7 +44,7 @@ export function Sidebar({ userRole }: SidebarProps) {
       {/* Menu de Navegação */}
       <nav className="flex-1 py-6 flex flex-col gap-2 px-3">
         
-        <NavItem to="/Dashboard" icon={<Home size={20} />} label="Início" collapsed={isCollapsed} active />
+        <NavItem to="/painel-regional" icon={<Home size={20} />} label="Início" collapsed={isCollapsed} active />
         <NavItem to="/zeladoria" icon={<FileText size={20} />} label="Zeladoria" collapsed={isCollapsed} />
         
         {userRole === 'admin' && (
